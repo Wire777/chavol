b0bf51f2d0dd79e72607b4d9a8d7eabd185fc882
Wire777 <isaacBarragan95@outlook.com>
2016-12-21 07:40:31 -0600

    Chavalillo

---
+++ Zapateria/src/zapateria/Login.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zapateria;
+
+/**
+ *
+ * @author isaac
+ */
+   
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4407576198809797110L;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	public Login() {
+		
+
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(12, 66, 70, 15);
+		add(lblUsuario);
+
+		JLabel lblIniciarSesionZapateria = new JLabel("Iniciar Sesion Zapateria");
+		lblIniciarSesionZapateria.setBounds(135, 12, 175, 15);
+		add(lblIniciarSesionZapateria);
+
+		textField = new JTextField();
+		textField.setBounds(97, 58, 242, 25);
+		add(textField);
+		textField.setColumns(10);
+
+		JLabel lblContrasea = new JLabel("Contraseña");
+		lblContrasea.setBounds(12, 107, 89, 15);
+		add(lblContrasea);
+
+		passwordField = new JPasswordField();
+		passwordField.setBounds(97, 95, 242, 29);
+		add(passwordField);
+
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBounds(107, 145, 117, 25);
+		add(btnEntrar);
+
+		JButton btnRestablecer = new JButton("Restablecer");
+		btnRestablecer.setBounds(241, 145, 134, 25);
+		add(btnRestablecer);
+
+	}
+}
+
+
